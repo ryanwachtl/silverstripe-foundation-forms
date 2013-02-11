3d6a76be556ae16e5831c197eb7277e55c028259
Ryan Wachtl <ryan.wachtl@gmail.com>
2013-02-11 14:17:07 -0600

    Minor update to _config.php

--- _config.php
+++ _config.php
@@ -8,22 +8,23 @@ $dir = basename(dirname(__FILE__));
 
 define('FOUNDATION_FORM_DIR', $dir);
 
-if(FOUNDATION_FORM_DIR != "bootstrap_forms") {
-	//user_error("The foundation_forms module must be in a directory named 'foundation_forms'",E_USER_ERROR);
+if(FOUNDATION_FORM_DIR != "foundationforms") {
+	user_error("The foundationforms module must be in a directory named 'foundationforms'",E_USER_ERROR);
 }
 
 if(!class_exists("GridField")) {
 	user_error("The foundation_forms module requires SilverStripe 3.0 or greater.", E_USER_ERROR);
 }
 
-// TODO Opt-in
+// TODO Opt-in Use
 Object::useCustomClass('MemberLoginForm', 'FoundationMemberLoginForm');
 
-// TODO Opt-in
+// TODO Opt-in Use
 if (class_exists('UserDefinedForm')) {
 	Object::add_extension('UserDefinedForm_Controller', 'FoundationUserDefinedForm_Controller');
 }
 
+// TODO Decorate Fields
 //Object::add_extension("FormField", "FoundationFormField");
 //Object::add_extension("TextField", "FoundationTextField");
 //Object::add_extension("OptionsetField", "FoundationOptionsetField");
