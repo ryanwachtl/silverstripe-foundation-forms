7a8df92417aa240fc1e1e03eab41c8a9e69d587c
Anselm Christophersen <ac@anselm.dk>
2015-01-07 15:01:11 +0100

    hooking into the silverstripe-comments module when installed

---
+++ code/FoundationCommentingController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Performs the FoundationFormTransformation on CommentingController
+ * @author Anselm Christophersen <ac@anselm.dk>
+ * @package foundation_forms
+ */
+class FoundationCommentingController extends DataExtension {
+
+	public function alterCommentForm(Form $form) {
+		$form->transform(new FoundationFormTransformation());
+		$form->setTemplate('FoundationCommentingControllerForm', 'FoundationForm', 'Form');
+		$form->addExtraClass('custom');
+	}
+
+}
